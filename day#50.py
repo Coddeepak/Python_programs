8b2402c3dfe168b899d2b24bb6c68f29be3cbcfc
Deepak Jaiswal <116238699+Coddeepak@users.noreply.github.com>
2023-07-17 18:02:59 +0545

    Add files via upload

---
+++ day#50.py
@@ -0,0 +1,30 @@
+    # Readline method
+
+f = open('myfile2.txt', 'r')
+while True:
+    line = f.readline()
+    print(line)
+    if not line:
+        print(line,type(line))
+        break
+
+f = open('myfile2.txt', 'r')
+i = 0
+while True:
+        i = i + 1
+        line = f.readline()
+        if not line:
+            break
+        m1 = int(line.split(",")[0])
+        m2 = int(line.split(",")[1])
+        m3 = int(line.split(",")[2])
+        print(f"Marks of student {i} in Maths is:{m1*2}")
+        print(f"Marks of student {i} in Science is:{m2*2}")
+        print(f"Marks of student {i} in Computer is:{m3*2}")
+
+    # Writeline method
+
+f = open('myfile3.txt','w')
+lines = ['line 1\n','line 2\n','line 3\n']
+f.writelines(lines)
+f.close()
